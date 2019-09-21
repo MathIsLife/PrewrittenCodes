01aba3fb5b996b7bb025865629b7336c503423bf
MathIsLife <jr_nirjhor@yahoo.com>
2019-09-21 21:22:55 +0600

    Update FloorSumOfArithmeticProgression.cpp

--- FloorSumOfArithmeticProgression.cpp
+++ FloorSumOfArithmeticProgression.cpp
@@ -21,14 +21,28 @@ ll get (ll x, ll n, ll m, ll lim) {
 
 // sum [(x + kn) / m] for 0 <= k < lim in O(lg max(n, m))
 inline ll floorAPsum (ll x, ll n, ll m, ll lim) {
+  ll ret = 0;
+  if (x < 0) {
+    ll q = x / m; x %= m;
+    if (x) x += m, --q;
+    ret += q * lim;
+  } if (n < 0) {
+    ll q = n / m; n %= m;
+    if (n) n += m, --q;
+    ret += lim * (lim - 1) * q / 2;
+  }
   ll tmp = aux(x, n, m), tot = lim / m;
-  ll ret = tmp * tot + tot * (tot - 1) * n * m / 2 + tot * n * (lim - tot * m);
+  ret += tmp * tot + tot * (tot - 1) * n * m / 2 + tot * n * (lim - tot * m);
   return ret + get(x, n, m, lim % m);
 }
 
 ll brute (ll x, ll n, ll m, ll lim) {
   ll ret = 0;
-  for (ll k = 0; k < lim; ++k) ret += (x + k * n) / m;
+  for (ll k = 0; k < lim; ++k) {
+    if ((x + k * n) % m == 0) ret += (x + k * n) / m;
+    else if (x + k * n < 0) ret += (x + k * n) / m - 1;
+    else ret += (x + k * n) / m;
+  }
   return ret;
 }
 
