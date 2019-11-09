aa753ae38819ad81df47d034c9086ca4395cd00c
MathIsLife <jr_nirjhor@yahoo.com>
2019-11-09 19:26:25 +0600

    Update FastFourierTransform.cpp

--- FastFourierTransform.cpp
+++ FastFourierTransform.cpp
@@ -25,6 +25,10 @@ struct cplx {
   const cplx operator / (const ld &x) const {
     return cplx(a / x, b / x);
   }
+
+  const cplx conj() const {
+    return cplx(a, -b);
+  }
 };
 
 const ld PI = acosl(-1);
@@ -33,40 +37,64 @@ const int N = (1 << 20) + 5;
 int rev[N]; cplx w[N];
 
 void prepare (int &n) { 
-  int sz = abs(31 - __builtin_clz(n));
-  cplx r = cplx(cosl(2 * PI / n), sinl(2 * PI / n)); 
-  w[0] = w[n] = 1; 
-  for (int i = 1; i < n; ++i) w[i] = w[i - 1] * r;
+  int sz = __builtin_ctz(n);
   for (int i = 1; i < n; ++i) rev[i] = (rev[i >> 1] >> 1) | ((i & 1) << (sz - 1));
+  w[0] = 0, w[1] = 1, sz = 1; 
+  while (1 << sz < n) {
+    cplx w_n = cplx(cosl(2 * PI / (1 << (sz + 1))), sinl(2 * PI / (1 << (sz + 1))));
+    for (int i = 1 << (sz - 1); i < (1 << sz); ++i) {
+      w[i << 1] = w[i], w[i << 1 | 1] = w[i] * w_n; 
+    } ++sz;
+  }
 }
 
-void fft (cplx *a, int n, int dir) { 
+void fft (cplx *a, int n) { 
   for (int i = 1; i < n - 1; ++i) { 
     if (i < rev[i]) swap(a[i], a[rev[i]]);
   }
-  for (int m = 2; m <= n; m <<= 1) {
-    for (int i = 0; i < n; i += m) {
-      for (int j = 0; j < (m >> 1); ++j) {
-        cplx &u = a[i + j], &v = a[i + j + (m >> 1)]; 
-        cplx t = v * w[dir ? n - n / m * j : n / m * j];
+  for (int h = 1; h < n; h <<= 1) {
+    for (int s = 0; s < n; s += h << 1) {
+      for (int i = 0; i < h; ++i) {
+        cplx &u = a[s + i], &v = a[s + i + h], t = v * w[h + i];
         v = u - t, u = u + t;
       }
     }
-  } if (dir) for (int i = 0; i < n; ++i) a[i] = a[i] / n;
+  }
 }
 
-cplx f_a[N], f_b[N];
+static cplx f[N];
 
 vector <ll> multiply (vector <ll> a, vector <ll> b) {
-  int sz = 1, n = a.size(), m = b.size();
+  int n = a.size(), m = b.size(), sz = 1;
   while (sz < n + m - 1) sz <<= 1; prepare(sz);
-  for (int i = 0; i < sz; ++i) f_a[i] = i < n ? cplx(a[i]) : cplx();
-  for (int i = 0; i < sz; ++i) f_b[i] = i < m ? cplx(b[i]) : cplx();
-  fft(f_a, sz, 0); fft(f_b, sz, 0);
-  for (int i = 0; i < sz; ++i) f_a[i] = f_a[i] * f_b[i];
-  fft(f_a, sz, 1); vector <ll> ret(n + m - 1);
-  for (int i = 0; i < n + m - 1; ++i) ret[i] = f_a[i].a + 0.3;
-  return ret;
+  for (int i = 0; i < sz; ++i) f[i] = cplx(i < n ? a[i] : 0, i < m ? b[i] : 0);
+  fft(f, sz);
+  for (int i = 0; i <= (sz >> 1); ++i) {
+    int j = (sz - i) & (sz - 1);
+    cplx x = (f[i] * f[i] - (f[j] * f[j]).conj()) * cplx(0, -0.25);
+    f[j] = x, f[i] = x.conj();
+  }
+  fft(f, sz); vector <ll> c(n + m - 1);
+  for (int i = 0; i < n + m - 1; ++i) c[i] = f[i].a / sz + 0.3;
+  return c;
+}
+
+int t, n;
+
+int main() {
+  cin >> t;
+  while (t--) {
+    cin >> n;
+    vector <ll> a(n + 1), b(n + 1);
+    for (int i = 0; i <= n; ++i) scanf("%lld", &a[i]);
+    for (int i = 0; i <= n; ++i) scanf("%lld", &b[i]);
+    vector <ll> c = multiply(a, b);
+    for (int i = 0; i <= n + n; ++i) {
+      if (i) printf(" "); printf("%lld", c[i]);
+    }
+    puts("");
+  }
+  return 0;
 }
 
 int main() {
