293a6e03ddb49a633bd4260c8d0010d1e56fb390
MathIsLife <jr_nirjhor@yahoo.com>
2020-07-18 07:52:42 +0600

    Update FloorSumOfArithmeticProgression.cpp

--- FloorSumOfArithmeticProgression.cpp
+++ FloorSumOfArithmeticProgression.cpp
@@ -4,56 +4,31 @@ using namespace std;
 
 typedef long long ll;
 
-// sum [(x + kn) / m] for 0 <= k < m
-inline ll aux (ll x, ll n, ll m) {
-  ll g = __gcd(n, m);
-  return g * (x / g) + (m * n - m - n + g) / 2;
+ll sum (ll n) {
+  return n * (n - 1) / 2;
 }
 
-// sum [(x + kn) / m] for 0 <= k < lim < m
-ll get (ll x, ll n, ll m, ll lim) {
-  if (!lim) return 0;
-  ll ret = lim * (x / m) + lim * (lim - 1) * (n / m) / 2; 
-  n %= m, x %= m; if (!n) return ret;
-  ll p = (x + (lim - 1) * n) / m;
-  return ret + p * lim - get(m - x + n - 1, m, n, p);
+// sum [(a + di) / m] for 0 <= i < n
+ll floorAPsum (ll a, ll d, ll m, ll n) {
+  ll res = d / m * sum(n) + a / m * n;
+  d %= m, a %= m; if (!d) return res;
+  ll to = (n * d + a) / m;
+  return res + (n - 1) * to - floorAPsum(m - 1 - a, m, d, to);
 }
 
-// sum [(x + kn) / m] for 0 <= k < lim in O(lg max(n, m))
-// m > 0, lim >= 0
-inline ll floorAPsum (ll x, ll n, ll m, ll lim) {
-  ll ret = 0;
-  if (x < 0) {
-    ll q = x / m; x %= m;
-    if (x) x += m, --q;
-    ret += q * lim;
-  } if (n < 0) {
-    ll q = n / m; n %= m;
-    if (n) n += m, --q;
-    ret += lim * (lim - 1) * q / 2;
-  }
-  ll tmp = aux(x, n, m), tot = lim / m;
-  ret += tmp * tot + tot * (tot - 1) * n * m / 2 + tot * n * (lim - tot * m);
-  return ret + get(x, n, m, lim % m);
-}
-
-ll brute (ll x, ll n, ll m, ll lim) {
-  ll ret = 0;
-  for (ll k = 0; k < lim; ++k) {
-    if ((x + k * n) % m == 0) ret += (x + k * n) / m;
-    else if (x + k * n < 0) ret += (x + k * n) / m - 1;
-    else ret += (x + k * n) / m;
-  }
-  return ret;
+// sum (a + di) % m for 0 <= i < n
+ll modAPsum (ll a, ll d, ll m, ll n) {
+  a = ((a % m) + m) % m, d = ((d % m) + m) % m;
+  return n * a + d * sum(n) - m * floorAPsum(a, d, m, n);
 }
 
 int main() {
-  ll x, n, m, lim;
-  while (cin >> x >> n >> m >> lim) {
-    ll p = brute(x, n, m, lim);
-    ll q = floorAPsum(x, n, m, lim);
-    cout << p << " " << q << endl;
-    assert(p == q);
+  int t; 
+  cin >> t;
+  while (t--) {
+    int n, m, a, b;
+    scanf("%d %d %d %d", &n, &m, &a, &b);
+    printf("%lld\n", floorAPsum(b, a, m, n));
   }
   return 0;
 }
