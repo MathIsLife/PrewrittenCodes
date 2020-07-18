53e43f27b47dff4440389de616ed6662e49f03df
MathIsLife <jr_nirjhor@yahoo.com>
2020-07-18 06:36:51 +0600

    Update PollardRho.cpp

--- PollardRho.cpp
+++ PollardRho.cpp
@@ -2,91 +2,68 @@
 
 using namespace std;
 
+typedef long long ll;
+typedef unsigned long long ull;
+
 namespace Rho {
-  template <typename num_t>
-  num_t mulmod (num_t a, num_t b, num_t p) {
-    a %= p; b %= p;
-    num_t q = (num_t) ((long double) a * b / p);
-    num_t r = a * b - q * p;
-    while (r < 0) r += p;
-    while (r >= p) r -= p;
-    return r;
+  ull mul (ull a, ull b, ull mod) {
+    ll ret = a * b - mod * (ull) (1.L / mod * a * b);
+    return ret + mod * (ret < 0) - mod * (ret >= (ll) mod);
   }
 
-  template <typename num_t>
-  num_t powmod (num_t n, num_t k, num_t p) {
-    num_t r = 1;
-    for (; k; k >>= 1) {
-      if (k & 1) r = mulmod(r, n, p);
-      n = mulmod(n, n, p);
+  ull bigMod (ull a, ull e, ull mod) {
+    ull ret = 1;
+    while (e) {
+      if (e & 1) ret = mul(ret, a, mod);
+      a = mul(a, a, mod), e >>= 1;
     }
-    return r;
+    return ret;
   }
-
-  template <typename num_t>
-  int rabin (num_t n) {
-    if (n == 2) return 1;
-    if (n < 2 || (~n & 1)) return 0;
-    const num_t p[9] = {2, 3, 5, 7, 11, 13, 17, 19, 23};
-    num_t a, d = n - 1, mx = 4;
-    int i, r, s = 0;
-    while (~d & 1) {++s; d >>= 1;}
-    for (i = 0; i < mx; ++i) {
-      if (n == p[i]) return 1;
-      if (!(n % p[i])) return 0;
-      a = powmod(p[i], d, n);
-      if (a ^ 1) {
-        for (r = 0; r < s && a != n - 1; ++r) a = mulmod(a, a, n);
-        if (r == s) return 0;
-      }
+    
+  bool isPrime (ull n) {
+    if (n < 2 or n % 6 % 4 != 1) return (n | 1) == 3;
+    ull a[] = {2, 325, 9375, 28178, 450775, 9780504, 1795265022};
+    ull s = __builtin_ctzll(n - 1), d = n >> s;
+    for (ull x : a) {
+      ull p = bigMod(x % n, d, n), i = s;
+      while (p != 1 and p != n - 1 and x % n and i--) p = mul(p, p, n);
+      if (p != n - 1 and i != s) return 0;
     }
     return 1;
   }
-
-  template <typename num_t>
-  inline num_t f (num_t a, num_t b, num_t n) {
-    return (mulmod(a, a, n) + b) % n;
+    
+  ull pollard (ull n) {
+    auto f = [&] (ull x) {return mul(x, x, n) + 1;};
+    ull x = 0, y = 0, t = 0, prod = 2, i = 1, q;
+    while (t++ % 40 or __gcd(prod, n) == 1) {
+      if (x == y) x = ++i, y = f(x);
+      if ((q = mul(prod, max(x, y) - min(x, y), n))) prod = q;
+      x = f(x), y = f(f(y));
+    }
+    return __gcd(prod, n);
   }
 
-  template <typename num_t>
-  void factorize (num_t n, vector <num_t> &facs) {
-    static int init_seed = 0;
-    if (!init_seed) {
-      init_seed = 1;
-      srand(2311);
-    }
-    if (n == 1) return;
-    if (rabin(n)) return void(facs.emplace_back(n));
-    if (n == 4) {
-      facs.emplace_back(2);
-      facs.emplace_back(2);
-      return;
-    }
-    while (1) {
-      num_t a = rand() & 63, x = 2, y = 2;
-      while (1) {
-        x = f(x, a, n), y = f(f(y, a, n), a, n);
-        if (x == y) break;
-        num_t p = __gcd(n, y <= x ? x - y : y - x);
-        if (p > 1) {
-          factorize(p, facs), factorize(n / p, facs);
-          return;
-        }
-      }
-    }
+  vector <ull> factor (ull n) {
+    if (n == 1) return {};
+    if (isPrime(n)) return {n};
+    ull x = pollard(n);
+    auto l = factor(x), r = factor(n / x);
+    l.insert(l.end(), r.begin(), r.end());
+    return l;
   }
-}
+};
 
-int t; long long n;
+int t; ll n;
 
 int main() {
-  srand(time(0));
-  scanf("%lld", &n);
-  vector <long long> facs;
-  Rho::factorize(n, facs);
-  map <long long, int> ppf;
-  for (auto it : facs) ++ppf[it];
-  for (auto it : ppf) cout << it.first << " " << it.second << '\n';
+  cin >> t;
+  while (t--) {
+    scanf("%lld", &n);
+    vector <ull> facs = Rho::factor(n);
+    sort(facs.begin(), facs.end());
+    printf("%d", (int) facs.size());
+    for (auto it : facs) printf(" %llu", it);
+    puts("");
+  }
   return 0;
 }
-
