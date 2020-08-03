812f51a98c5fb63cae711163558726c9442eb0fe
MathIsLife <jr_nirjhor@yahoo.com>
2020-08-03 08:05:26 +0600

    Create FractionClass.cpp

---
+++ FractionClass.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+struct frac {
+  ll u, d;
+
+  frac (ll _u = 0, ll _d = 1) {
+    u = _u, d = _d;
+    ll g = __gcd(abs(u), abs(d));
+    u /= g, d /= g;
+    if (d < 0) u = -u, d = -d;
+  }
+
+  inline void print() {
+    cerr << u << "/" << d << '\n';
+  }
+
+  frac operator + (const frac &f) const {
+    return frac(u * f.d + d * f.u, d * f.d);
+  }
+
+  frac operator - (const frac &f) const {
+    return frac(u * f.d - d * f.u, d * f.d);
+  }
+
+  frac operator * (const frac &f) const {
+    return frac(u * f.u, d * f.d);
+  }
+
+  frac operator / (const frac &f) const {
+    return frac(u * f.d, d * f.u);
+  }
+  
+  bool operator < (const frac &f) const {
+    return u * f.d < d * f.u;
+  }
+
+  bool operator <= (const frac &f) const {
+    return u * f.d <= d * f.u;
+  }
+  
+  bool operator > (const frac &f) const {
+    return u * f.d > d * f.u;
+  }
+
+  bool operator >= (const frac &f) const {
+    return u * f.d >= d * f.u;
+  }
+
+  bool operator == (const frac &f) const {
+    return u * f.d == d * f.u;
+  }
+};
+
+int main() {
+
+  return 0;
+}
+
