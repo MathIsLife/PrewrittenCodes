710a844c00ebfda2bbd829ec842ea992ae346763
Jubayer Nirjhor <jr_nirjhor@yahoo.com>
2021-02-09 00:36:14 +0600

    Create BigMultiply.cpp

---
+++ BigMultiply.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+
+ull bigMultiply (ull a, ull b, ull M) {
+  ll ret = a * b - M * ull(ld(a) * ld(b) / ld(M));
+  return ret < 0 ? ret + M : (ret >= ll(M) ? ret - M : ret);
+}
+
+int main() {
+  
+  return 0;
+}
+
