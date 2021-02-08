0a8599c0164bd8492ff9f8afb80facaeb1f38dc3
Jubayer Nirjhor <jr_nirjhor@yahoo.com>
2021-02-09 00:36:49 +0600

    Update BigMultiply.cpp

--- BigMultiply.cpp
+++ BigMultiply.cpp
@@ -6,6 +6,7 @@ typedef long long ll;
 typedef long double ld;
 typedef unsigned long long ull;
 
+// a and b can be long long 
 inline ull bigMultiply (ull a, ull b, ull M) {
   ll ret = a * b - M * ull(ld(a) * ld(b) / ld(M));
   return ret < 0 ? ret + M : (ret >= ll(M) ? ret - M : ret);
